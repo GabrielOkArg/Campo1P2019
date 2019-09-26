619e7dce7b93725f094a67f4e57497f8b7012a13
GabrielOkArg <eduardo.silva@neoris.com>
2019-09-26 16:03:43 -0300

    Agregar archivos de proyecto.

---
+++ BE/CajaAhorro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class CajaAhorro
+    {
+        public int NumCaja { get; set; }
+        public int Saldo { get; set; }
+    }
+}
---
+++ BLL/OperacionesLogic.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+
+namespace BLL
+{
+    public class OperacionesLogic
+    {
+
+
+        public bool Depositar(CajaAhorro cajaAhorro, int monto)
+        {
+            try
+            {
+                cajaAhorro.Saldo += monto;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+                throw ex;
+            }
+        }
+
+        public bool Extraer(CajaAhorro cajaAhorro, int monto)
+        {
+            try
+            {
+                if (cajaAhorro.Saldo < monto)
+                {
+                    return false;
+                    throw new Exception("El saldo es insuficiente para realizar la operacion");
+                }
+                else
+                {
+                    cajaAhorro.Saldo -= monto;
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+        }
+
+        public bool Transferir(CajaAhorro cajaAhorro, CajaAhorro cajaAhorro1, int monto)
+        {
+            try
+            {
+                bool TraferenciaValida = this.Extraer(cajaAhorro, monto);
+                if (TraferenciaValida)
+                {
+                    cajaAhorro1.Saldo += monto;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+        }
+    }
+}
